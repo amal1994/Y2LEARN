13615e4edf574fb3895c354dba232781b06081b6
Amal Shukla <amal1994@users.noreply.github.com>
2015-11-09 14:32:53 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,56 @@
+<div id="block_13"><div class="c">
+<div class="r">MarketPlace Categories</div>
+<a href="" title="Premium Drupal Themes">Drupal Themes</a>
+<a href="" title="Premium WordPress Themes">WordPress Themes</a>
+<a href="" title="Premium HTML/CSS Website Templates">HTML/CSS Templates</a>
+<a href="" title="Premium PSD Templates">PSD Templates</a>
+</div>
+
+<div class="c x">
+<div class="r">Follow &amp; Keep in Touch</div>
+<div class="xx">
+<a href=""  title="Facebook Page">Facebook</a>
+</div>
+<div class="xx">
+<a href=""  title="Twitter Account">Twitter</a>
+</div>
+<div class="xx">
+<a href=""  title="Blog RSS Feed">Blog RSS Feed</a>
+</div>
+<div class="xx">
+<a href="">Contact Us</a>
+</div>
+<div class="xx">
+<a href=""  title="Blog">Blog</a>
+</div>
+<div class="small">
+<img src="<?php echo get_template_directory_uri(); ?>/images/TechAge-Logo-11-300x103.png">
+<img src="<?php echo get_template_directory_uri(); ?>/images/TechAge-Logo-11-300x103.png">
+<img src="<?php echo get_template_directory_uri(); ?>/images/TechAge-Logo-11-300x103.png">
+<img src="<?php echo get_template_directory_uri(); ?>/images/TechAge-Logo-11-300x103.png">
+</div>
+</div>
+<div class="c">
+<div class="r">Useful Links</div>
+<a href="">Become an Author</a>
+<a href="">Affiliate Program</a>
+<a href="">Author Guidelines</a>
+<a href="">Terms &amp; Conditions</a>
+</div>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','js\onolitegz.js','ga');
+
+  ga('create', 'UA-3428052-5', { 'cookieDomain': 'www.techagelabs.com'});
+  ga('send', 'pageview');
+
+</script></div><div id="footer">
+                                           &copy; 2015 TechAge | techagelabs.com <br/>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
