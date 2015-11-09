92702453df784b7972fded7791b14a6d3d908690
Amal Shukla <amal1994@users.noreply.github.com>
2015-11-09 14:34:13 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html><html>
+
+<head profile="http://www.w3.org/1999/xhtml/vocab">
+
+	<meta name="description" content="The Y2LEARN theme from techage is the most feature-rich Pinterest clone online. It&#039;s open source, PHP, and allows for a Pinterest-styled layout." />
+	<meta name="keywords" content="pinterest, clone, pin images, pinned, pinboard, template, cms, php, open source" />
+	<meta name="robots" content="index,follow" />    <meta name=viewport content="width=960px, initial-scale=0.5">
+	<script src="<?php echo get_template_directory_uri();?>/js/ajax.js" type="text/javascript"></script>
+    <link rel="shortcut icon" href="http://techagelabs.com/blog/wp-content/uploads/2015/02/TechAge-Logo-11-300x103.png" type="image/x-icon" />
+    <title>Y2LEARN</title>
+    <link href="<?php echo get_template_directory_uri(); ?>/font-awesome-4.3.0/css/font-awesome.css"  rel="stylesheet"/> 
+    <link href="<?php echo get_template_directory_uri(); ?>/css/header.css" rel="stylesheet" />
+    <link href="<?php echo get_template_directory_uri(); ?>/css/login-form.css" rel="stylesheet" />
+<script src="http://code.jquery.com/jquery-1.8.3.min.js" type="text/javascript"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js" type="text/javascript"></script>
+</head>
+<body class="html front not-logged-in no-sidebars page-node" >
+<div id="page-wrapper"><div id="page" itemscope itemtype="http://schema.org/CreativeWork" >
+    <div id="header">
+        <div id="logo" class="section clearfix">
+           <div class="menu clear-block"><div class="frame">
+                <a href="index.php"><img src="http://img.static.xmemart.in/Tham2bfc6aaa-0ce9-482f-a40e-ba21f512f5874f3142f2-f1ba-46ed-b587-782c7c935475TechAgeLogo.jpg"></a>
+            </div><div id="usermenu" class="menu"><a href="http://localhost/wordpress-new/home/"><i class='fa fa-home' style='width:25px;'></i> <u>HOME</u></a><a href='http://localhost/wordpress-new/posting-page/' ><i class='fa fa-drupal' style='width:14px;'></i><u>POST</u></a><a href='http://localhost/wordpress-new/single-post/' ><i class='fa fa-wordpress' style='width:14px;'></i> <u>Single Post</u></a><a href='http://techagelabs.com/blog/' ><i class='fa fa-rss' style='width:14px;'></i><u> Blog </u></a><a href='http://localhost/wordpress-new/contact-us/' ><i class='fa fa-envelope' style='width:14px;'></i><u> Contact Us </u></a></div></div></div>
